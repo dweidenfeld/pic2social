ec67e8f2424e90e8eed3f7032326bb0ae0795f45
Dominik Weidenfeld <dominik.weidenfeld@twt.de>
2018-04-10 07:36:02 +0200

    added clippy / externalized doc / added bitbucket pipeline

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
+# Pic2Social
+
+## Description
+This crate is a image upload for social media portals.
+
+```
+pic2social 0.1.0
+Dominik Weidenfeld <dominik@sh0k.de>
+A social media image uploader with active directory watching
+
+USAGE:
+    pic2social --directory <directory> --message <message>
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -d, --directory <directory>
+    -m, --message <message>
+```
+
+You can run it with
+```
+./pic2social -d pic/ -m "#myHashTag"
+```
+
+Then it will watch the pic folder and everytime a picture is added
+to the directory it will be uploaded with the provided message.
+
+## Cross Compilation
 `cross build --target=x86_64-pc-windows-gnu`
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,7 @@
-//! This crate is a image upload for social media portals.
-//!
-//! ```
-//! pic2social 0.1.0
-//! Dominik Weidenfeld <dominik@sh0k.de>
-//! A social media image uploader, that watches a directory
-//!
-//! USAGE:
-//!     pic2social --directory <directory> --message <message>
-//!
-//! FLAGS:
-//!     -h, --help       Prints help information
-//!     -V, --version    Prints version information
-//!
-//! OPTIONS:
-//!     -d, --directory <directory>
-//!     -m, --message <message>
-//! ```
-//!
-//! You can run it with
-//! ```
-//! ./pic2social -d pic/ -m "#myHashTag"
-//! ```
-//!
-//! Then it will watch the pic folder and everytime a picture is added
-//! to the directory it will be uploaded with the provided message.
-#![cfg_attr(feature="clippy", feature(plugin))]
-#![cfg_attr(feature="clippy", plugin(clippy))]
+#![cfg_attr(test, feature(plugin))]
+#![cfg_attr(test, plugin(clippy))]
+#![feature(external_doc)]
+#![doc(include="../README.md")]
 
 extern crate egg_mode;
 extern crate dotenv;
@@ -36,7 +12,7 @@ extern crate structopt;
 #[macro_use]
 extern crate error_chain;
 
-mod social;
+pub mod social;
 mod watchdir;
 
 use std::cell::RefCell;
@@ -49,7 +25,7 @@ use structopt::StructOpt;
 /// The configuration object that holds all external configuration definitions
 /// It will be automatically parsed using `StructOpt`.
 #[derive(StructOpt, Debug)]
-#[structopt(name = "pic2social", about = "A social media image uploader, that watches a directory")]
+#[structopt(name = "pic2social", about = "A social media image uploader with active directory watching")]
 struct Config {
     #[structopt(short = "d", long = "directory", parse(from_os_str))]
     directory: PathBuf,
@@ -64,10 +40,10 @@ fn main() {
     dotenv::dotenv()
         .expect("could not parse environment variables");
 
-    let core = reactor::Core::new().ok()
+    let core = reactor::Core::new()
         .expect("could not initialize core");
 
-    let upload = social::twitter::Twitter::new(RefCell::new(core)).ok()
+    let upload = social::twitter::Twitter::new(RefCell::new(core))
         .expect("Twitter client could not be initialized");
 
     println!("Watching for pictures in directory: {}", conf.directory.display());
@@ -76,7 +52,7 @@ fn main() {
             match upload.post(conf.message.clone(), path.as_path()) {
                 Ok(_) => println!("Uploaded Image: {} ({})", conf.message, path.display()),
                 Err(e) => println!("Error uploading image: {} ({}) {:?}", conf.message, path.display(), e)
-         c   };
+            };
         }
     }) {
         Ok(_) => (),
